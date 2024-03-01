076fbad1639520f67814edcd790f499696fe1c49
Matthew J. H. Peronto <matthew.peronto@gmail.com>
2024-03-01 03:20:48 -0800

    Initial Commit
    * Updates text to match effects when combined with "Sukritact's Oceans"

---
+++ README.md
@@ -0,0 +1,14 @@
+# GrayPockets - Deep Sea Fisheries - Cooks
+
+## Description
+
+This mod cooks several items in "Deep Sea Fisheries"
+
+* Updates text to match effects when combined with "Sukritact's Oceans"
+
+## Requirements
+
+* Deep Sea Fisheries
+* Sukritact's Oceans
+
+
---
+++ SQL/Text/DeepSeaFisheriesCooks.sql
@@ -0,0 +1,6 @@
+-- Support Sukritact's Oceans
+
+INSERT OR REPLACE INTO LocalizedText
+	(	"Language",	"Tag",									"Text"																																																																																																																							)
+VALUES
+	(	'en_US',	'LOC_IMPROVEMENT_FISHERY_DESCRIPTION',	'A [ICON_Governor] Governor unique improvement that can be built in a city with a Surveyor Governor with the Aquaculture promotion.[NEWLINE][NEWLINE]This tile cannot be worked. Fishing Boats adjacent to one or more Fisheries yield +2 [ICON_Food] Food; and another +1 [ICON_Production] Production while Liang is established in this city. +1 [ICON_Housing] Housing. Must be placed on a Coast, Lake, or Ocean tile adjacent to a sea resource. May be built on Kelp Forests and Reefs.'	);
